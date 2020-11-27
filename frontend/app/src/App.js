05fc1ee9a55208cc74c4d2695893bdb55d90d9ad
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-11-26 21:37:45 -0500

    fixed hamburger navbar menu

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,64 +1,19 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-class SongIntake extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      title: '',
-      artist: '',
-      album: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch("http://127.0.0.1:5000/insert-song", {
-      method: "POST",
-      body: JSON.stringify({
-        title: this.state.title,
-        artist: this.state.artist,
-        album: this.state.album
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-      .then(response => response.json())
-      .then(json => console.log(json));
-  }
-
-  handleClick(event) {
-
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label>Title</label>
-          <input type="text" name="title" onChange={this.handleChange} />
-        </div>
-        <div>
-          <label>Album</label>
-          <input type="text" name="album" onChange={this.handleChange} />
-        </div>
-        <div>
-          <label>Artist</label>
-          <input type="text" name="artist" onChange={this.handleChange} />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
-export default SongIntake;
+export default App;
--- frontend/app/src/components/Navbar.js
+++ frontend/app/src/components/Navbar.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button'
 
 function Navbar() {
   const [click, setClick] = useState(false);
-
   const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
-      setButton(false)
+      setButton(false);
     } else {
-      setButton(true)
+      setButton(true);
     }
   };
+
+  useEffect(() => {
+    showButton();
+  }, []);
 
   window.addEventListener('resize', showButton);
 
@@ -26,30 +28,44 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
-            FULL STACK <i className='fab fa-typo3' />
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            HAUS
+            <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
-          <ul className={click ? 'nav - menu active' : 'nav-menu'}>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
                 Services
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
                 Products
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
                 Sign Up
               </Link>
             </li>
@@ -58,7 +74,7 @@ function Navbar() {
         </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
